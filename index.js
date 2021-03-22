266f9f11309cb15a1e3ca73e2f7e8d83b796929f
APratham <a.pratham1998@gmail.com>
2021-03-22 16:09:40 +0530

    Added Cloud Run app

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const lib = require('thoughts');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
+var random = lib.random();    // gives random object from any category
+var anotherRandom = lib.random("anonymous");  //gives random object from specified category
+var particular = lib.particular("anonymous",2);   //gives particular object from specified category
 
-  const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}!`);
+app.get('/', (req, res) => {
+  console.log('The app received a request.');
+  res.send(random.thought);
 });
 
 const port = process.env.PORT || 8080;
